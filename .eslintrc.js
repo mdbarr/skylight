00bf9451f8ae9a8fe243edc4f4a554cac2e0712f
Mark Barr <mbarr@veobot.com>
2019-01-11 12:08:02 -0500

    Vuetify bootstrapping.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,12 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    'template-curly-spacing': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
